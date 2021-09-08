b3fd4e07783bd57e1268af9d1d68a3c03e047633
Sahil Saha <sahilsaha7773@gmail.com>
2021-09-08 20:34:29 +0530

    Init and added cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import AppCard from './components/AppCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h1>Last Bench Devs App Store</h1>
+      <div className="app-list">
+        <AppCard
+          appName="App Name"
+          appBanner="https://media.discordapp.net/attachments/844194529263616042/885171770129276928/sponline_phone15_generated.jpg?width=1101&height=661"
+          appIcon="https://i.pinimg.com/736x/a4/d4/80/a4d4803a121a6dd1d7742b57897e4dbc.jpg"
+          appDescription="Ipsum magna Lorem Lorem velit labore amet laborum incididunt laborum do do. labore amet laborum incididunt laborum do do. "
+        />
+        <AppCard
+          appName="App Name"
+          appBanner="https://dbwgapw6amg93.cloudfront.net/wp-content/uploads/2016/10/best_mobile_app_UI_2016_Personal_Festival.jpg"
+          appIcon="https://i.pinimg.com/736x/a4/d4/80/a4d4803a121a6dd1d7742b57897e4dbc.jpg"
+          appDescription="Ipsum magna Lorem Lorem velit labore amet laborum incididunt laborum do do. labore amet laborum incididunt laborum do do. "
+        />
+        <AppCard
+          appName="App Name"
+          appBanner="https://blog.tubikstudio.com/wp-content/uploads/2019/03/blog_app_ui_design_tubik.png"
+          appIcon="https://i.pinimg.com/736x/a4/d4/80/a4d4803a121a6dd1d7742b57897e4dbc.jpg"
+          appDescription="Ipsum magna Lorem Lorem velit labore amet laborum incididunt laborum do do. labore amet laborum incididunt laborum do do. "
+        />
+
+        <AppCard
+          appName="App Name"
+          appBanner="https://blog.tubikstudio.com/wp-content/uploads/2019/03/blog_app_ui_design_tubik.png"
+          appIcon="https://i.pinimg.com/736x/a4/d4/80/a4d4803a121a6dd1d7742b57897e4dbc.jpg"
+          appDescription="Ipsum magna Lorem Lorem velit labore amet laborum incididunt laborum do do. labore amet laborum incididunt laborum do do. "
+        />
+        <AppCard
+          appName="App Name"
+          appBanner="https://media.discordapp.net/attachments/844194529263616042/885171770129276928/sponline_phone15_generated.jpg?width=1101&height=661"
+          appIcon="https://i.pinimg.com/736x/a4/d4/80/a4d4803a121a6dd1d7742b57897e4dbc.jpg"
+          appDescription="Ipsum magna Lorem Lorem velit labore amet laborum incididunt laborum do do. labore amet laborum incididunt laborum do do. "
+        />
+        <AppCard
+          appName="App Name"
+          appBanner="https://dbwgapw6amg93.cloudfront.net/wp-content/uploads/2016/10/best_mobile_app_UI_2016_Personal_Festival.jpg"
+          appIcon="https://i.pinimg.com/736x/a4/d4/80/a4d4803a121a6dd1d7742b57897e4dbc.jpg"
+          appDescription="Ipsum magna Lorem Lorem velit labore amet laborum incididunt laborum do do. labore amet laborum incididunt laborum do do. "
+        />
+      </div>
+      <div style={{ margin: "100px auto", textAlign: 'center', width: 'fit-content' }}>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/AppCard.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import '../styles/AppCard.css';
+import androidIcon from '../images/android.png';
+import iosIcon from '../images/apple.png';
+
+function AppCard({
+  appName,
+  appDescription,
+  appBanner,
+  androidLink,
+  iosLink,
+  directLink,
+  appIcon
+}) {
+  return (
+    <div className="card-container">
+      <img className="card-banner" src={appBanner} />
+      <div className="gradient-top">
+        <img src={appIcon} />
+        <h2>
+          {appName}
+        </h2>
+      </div>
+      <div className="gradient-bottom">
+        <p>{appDescription}</p>
+        <div className="card-toolbar">
+          {/* <a>
+            <img className="card-icon" src={androidIcon} />
+          </a>
+          <a>
+            <img className="card-icon" src={iosIcon} />
+          </a> */}
+          <img className="card-icon" src="https://media.discordapp.net/attachments/844194529263616042/885175042210676756/1200px-Circle-icons-download.svg.png?width=661&height=661" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AppCard
